cc09b17c7baac5ea2c374107159ad891f1871a3c
Junwen Qin <jwqin@thoughtworks.com>
2016-09-08 00:00:55 +0800

    refactor code; add watch

--- index.js
+++ index.js
@@ -1,78 +1,75 @@
-function processToken(o1, operators) {
-  const [o2, ...rest] = operators;
+/*
+* 只考虑四则运算的简化版逆波兰计算器
+* 运算符转换规则: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+* */
+(function(global) {
+  const OPERATOR_PRECEDENCE = [['-', '+'], ['*', '/']]; //定义运算符与优先级, 优先级相同的运算符在同一数组内
 
-  if (o2 && compare(o1, o2)) {
-    const [result, nextOps] = processToken(o1, rest);
+  const operatorFuncs = {
+    '+' : a => b => a + b,
+    '-' : a => b => a - b,
+    '*' : a => b => a * b,
+    '/' : a => b => a / b,
+  };
 
-    return [[o2].concat(result), nextOps]
-  } else {
-    return [[], [o1].concat(operators)];
-  }
-}
+  const OPERATORS = OPERATOR_PRECEDENCE.reduce((acc, group)=> acc.concat(group), []);
 
-var opsDef = [['-', '+'], ['*', '/']];
-function compare(o1, o2) {
-  return findPriv(o1) <= findPriv(o2);
-}
+  function processToken(o1, operators) {
+    const [o2, ...rest] = operators;
 
-function findPriv(o) {
-  return opsDef.findIndex(function(v, i) {return v.includes(o)})
-}
+    if (o2 && isPrecedenceLessThan(o1, o2)) {
+      const [result, nextOps] = processToken(o1, rest);
 
-function convert(input = [], operators = []) {
-  const [token, ...rest] = input;
-
-  if (token === undefined) {return operators};
-
-  if (/^\d+$/.test(token)) {
-    return [parseInt(token)].concat(convert(rest, operators));
-  } else {
-    const [nextValues, nextOperators] = processToken(token, operators);
-
-    return nextValues.concat(convert(rest, nextOperators));
-  }
-}
-
-
-const fns = {
-  '+' : a => b => a + b,
-  '-' : a => b => a - b,
-  '*' : a => b => a * b,
-  '/' : a => b => a / b,
-};
-
-function isOperator(i) {return '+-*/'.indexOf(i) !== -1}
-
-function solveRPN(input) {
-  return input.reduce((acc, v, i) => {
-    // console.log(acc, v)
-    if (isOperator(v)) {
-      const a = acc[acc.length - 1], b = acc[acc.length -2];
-      return [...acc.slice(0, acc.length - 2), fns[v](b)(a)]
+      return [[o2].concat(result), nextOps]
     } else {
-      return [...acc, v]
+      return [[], [o1].concat(operators)];
     }
-  }, [])
-}
-
-function solveRPN2(input, stack = []) {
-  // console.log('in', input, stack)
-  if(input.length === 0) return stack;
-
-  const [a, ...rest] = input;
-
-  if (isOperator(a)) {
-    const [x, y, ...restStack] = stack;
-    const fn = fns[a];
-    return solveRPN2(rest, [fn(y)(x), ...restStack]);
-  } else {
-    return solveRPN2(rest, [a, ...stack]);
   }
-}
 
-const rpn = convert(["1", "+", "2", "*", "3", "-", "4", "*", "2"]);
 
-console.log(rpn)
-// console.log()
-console.log('1', solveRPN(rpn));
-console.log('2', solveRPN2(rpn));
+  function isPrecedenceLessThan(o1, o2) {
+    return findPrecedence(o1) <= findPrecedence(o2);
+  }
+
+  function findPrecedence(o) {
+    return OPERATOR_PRECEDENCE.findIndex(function(v, i) {return v.includes(o)})
+  }
+
+  function convertToRPN(input = [], operators = []) {
+    const [token, ...rest] = input;
+
+    if (token === undefined) {return operators};
+
+    if (/^\d+$/.test(token)) {
+      return [parseInt(token)].concat(convertToRPN(rest, operators));
+    } else {
+      const [nextValues, nextOperators] = processToken(token, operators);
+
+      return nextValues.concat(convertToRPN(rest, nextOperators));
+    }
+  }
+
+  function isOperator(i) {
+    return OPERATORS.includes(i)
+  }
+
+  function solveRPN(input, stack = []) {
+    if(input.length === 0) return stack;
+
+    const [a, ...rest] = input;
+
+    if (isOperator(a)) {
+      const [x, y, ...restStack] = stack;
+      const fn = operatorFuncs[a];
+      return solveRPN(rest, [fn(y)(x), ...restStack]);
+    } else {
+      return solveRPN(rest, [a, ...stack]);
+    }
+  }
+
+  function calculate (input) {
+    return solveRPN(convertToRPN(input))
+  }
+
+  global.calculate = calculate;
+})(window);
